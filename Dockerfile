0949cce86a42640f9f81fbe52eba4044d08b509c
“chiupam” <chiupam@126.com>
2021-08-14 12:37:09 +0800

    提交

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,18 @@
 FROM centos
 
-COPY google-chrome.rpm chromedriver /root/
+COPY * /root/
 
 RUN yum -y update && \
 yum -y groupinstall development  && \
 yum -y install epel-release && \
 yum -y install python36 && \
+yum -y install crontabs && \
 pip3 install selenium && \
 pip3 install requests && \
-yum -y install crontabs && \
 yum -y localinstall /root/google-chrome.rpm && \
 chmod +x /root/chromedriver && \
-mkdir /demo
+mkdir /demo && \
+mv /root/example.py /demo
 
 WORKDIR /demo
 
--- example.py
+++ example.py
@@ -1,17 +1,16 @@
 # coding=utf-8
-from selenium import webdriver
+from selenium import webdriver  # 必须
 
 
 def main():
-    url = 'https://www.baidu.com/'
-    driver.get(url=url)
+    driver.get('https://www.baidu.com/')
     print(driver.title)
     driver.quit()
 
 
 if __name__ == "__main__":
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path= '/root/chromedriver', options=chrome_options)
+    chrome_options = webdriver.ChromeOptions()  # 必须
+    chrome_options.add_argument('--headless')  # 必须
+    chrome_options.add_argument('--no-sandbox')  # 必须
+    driver = webdriver.Chrome(executable_path='/root/chromedriver', options=chrome_options)  # 必须
     main()
