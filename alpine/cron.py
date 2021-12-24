398064fcd160b89ace23ba7c27b6fe3d29cad2be
chiupam <chiupam@126.com>
2021-12-24 14:19:05 +0800

    更新

--- alpine/cron.py
+++ alpine/cron.py
@@ -6,14 +6,8 @@ from os import system
 
 while True:
     try:
-        with open("/data/cron.sh", "r", encoding="utf-8") as f1:
-            t1 = f1.read()
-        sleep(2)
-        with open("/data/cron.sh", "r", encoding="utf-8") as f2:
-            t2 = f2.read()
-        if t1 != t2:
-            system("crontab /data/cron.sh")
+        system("crontab /data/cron.sh")
     except FileNotFoundError:
-        sleep(2)
         continue
-        
+    sleep(5)
+    
